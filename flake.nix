7ae0d05b0c41903c222f71a735d6bcb4d0f4b2ec
default <info@kajjagtenberg.nl>
2025-09-07 12:45:48 +0000

    chore: add example nixos configuration

--- flake.nix
+++ flake.nix
@@ -5,11 +5,27 @@
     nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
   };
 
-  outputs = { self, nixpkgs }: {
+  outputs =
+    { self, nixpkgs, ... }@inputs:
+    let
+      inherit (self) outputs;
+      vars = import ./vars.nix;
 
-    packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
+      mkNixOSConfig =
+        path:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs vars; };
+          modules = [ path ];
+        };
+    in
+    {
 
-    packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+      # packages.x86_64-linux.hello = nixpkgs.legacyPackages.x86_64-linux.hello;
 
-  };
+      # packages.x86_64-linux.default = self.packages.x86_64-linux.hello;
+
+      nixosConfigurations = {
+        example = mkNixOSConfig ./machines/example/configuration.nix;
+      };
+    };
 }
