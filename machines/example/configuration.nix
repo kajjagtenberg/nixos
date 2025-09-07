3a4f7c639e090d871173ee5823ca53221ff30749
default <info@kajjagtenberg.nl>
2025-09-07 13:48:58 +0000

    chore: favor home manager over system packages

--- machines/example/configuration.nix
+++ machines/example/configuration.nix
@@ -80,14 +80,12 @@
 
   # List packages installed in system profile. To search, run:
   # $ nix search wget
-  environment.systemPackages = with pkgs; [
-    #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-    #  wget
-    vim
-    curl
-    wget
-    htop
-  ];
+  # environment.systemPackages = with pkgs; [
+  #   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
+  #   #  wget
+  #   vim
+  #   htop
+  # ];
 
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
--- modules/home-manager/base.nix
+++ modules/home-manager/base.nix
@@ -3,15 +3,26 @@
   ...
 }:
 {
+  imports = [
+    ./packages.nix
+  ];
+
   home = {
     username = vars.username;
     # homeDirectory = lib.mkMerge [
     #   (lib.mkIf pkgs.stdenv.isLinux "/home/${vars.userName}")
     #   (lib.mkIf pkgs.stdenv.isDarwin "/Users/${vars.userName}")
     # ];
+
     stateVersion = "25.05";
     # sessionVariables = lib.mkIf pkgs.stdenv.isDarwin {
     #   SOPS_AGE_KEY_FILE = "$HOME/.config/sops/age/keys.txt";
     # };
+  };
+
+  programs = {
+    htop.enable = true;
+    vim.enable = true;
+    fzf.enable = true;
   };
 }
