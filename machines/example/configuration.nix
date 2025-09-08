467d092a34c749fa286c8bd87b79429fd847fac5
default <info@kajjagtenberg.nl>
2025-09-08 16:10:18 +0000

    chore: cleanup

--- machines/example/configuration.nix
+++ machines/example/configuration.nix
@@ -47,24 +47,6 @@
     variant = "";
   };
 
-  # # Define a user account. Don't forget to set a password with ‘passwd’.
-  # users.users = {
-  #   ${vars.username} = {
-  #     isNormalUser = true;
-  #     # description = "Kaj";
-  #     extraGroups = [
-  #       "networkmanager"
-  #       "wheel"
-  #     ];
-  #     packages = with pkgs; [
-  #       mergerfs
-  #       snapraid
-  #       neovim
-  #     ];
-  #     imports = [];
-  #   };
-  # };
-
   home-manager = {
     extraSpecialArgs = { inherit inputs outputs vars; };
     useGlobalPkgs = true;
@@ -79,37 +61,5 @@
     };
   };
 
-  # List packages installed in system profile. To search, run:
-  # $ nix search wget
-  # environment.systemPackages = with pkgs; [
-  #   #  vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
-  #   #  wget
-  #   vim
-  #   htop
-  # ];
-
-  # Some programs need SUID wrappers, can be configured further or are
-  # started in user sessions.
-  # programs.mtr.enable = true;
-  # programs.gnupg.agent = {
-  #   enable = true;
-  #   enableSSHSupport = true;
-  # };
-
-  # List services that you want to enable:
-
-  # Open ports in the firewall.
-  # networking.firewall.allowedTCPPorts = [ ... ];
-  # networking.firewall.allowedUDPPorts = [ ... ];
-  # Or disable the firewall altogether.
-  # networking.firewall.enable = false;
-
-  # This value determines the NixOS release from which the default
-  # settings for stateful data, like file locations and database versions
-  # on your system were taken. It‘s perfectly fine and recommended to leave
-  # this value at the release version of the first install of this system.
-  # Before changing this value read the documentation for this option
-  # (e.g. man configuration.nix or on https://nixos.org/nixos/options.html).
   system.stateVersion = "25.05"; # Did you read the comment?
-
 }
