7671fe066df40f0505828c2808aa5eef25edf188
default <info@kajjagtenberg.nl>
2025-09-11 20:12:37 +0000

    chore: move xserver keymap to correct module

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -35,6 +35,18 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  i18n.extraLocaleSettings = {
+    LC_ADDRESS = "nl_NL.UTF-8";
+    LC_IDENTIFICATION = "nl_NL.UTF-8";
+    LC_MEASUREMENT = "nl_NL.UTF-8";
+    LC_MONETARY = "nl_NL.UTF-8";
+    LC_NAME = "nl_NL.UTF-8";
+    LC_NUMERIC = "nl_NL.UTF-8";
+    LC_PAPER = "nl_NL.UTF-8";
+    LC_TELEPHONE = "nl_NL.UTF-8";
+    LC_TIME = "nl_NL.UTF-8";
+  };
+
   nix = {
     gc = {
       automatic = true;
@@ -48,6 +60,12 @@
       ];
       auto-optimise-store = true;
     };
+  };
+
+  # Configure keymap in X11
+  services.xserver.xkb = {
+    layout = "us";
+    variant = "";
   };
 
   system.stateVersion = "25.05";
