9091d9127bc2d27431b041b0e9e81fa296e6236a
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-13 15:42:47 +0200

    chore: add nfs share to swarm nodes

--- machines/swrm3/configuration.nix
+++ machines/swrm3/configuration.nix
@@ -33,5 +33,14 @@
     };
   };
 
+  fileSystems."/mnt/data" = {
+    device = "10.0.0.94:/mnt/fast/k8s";
+    fsType = "nfs";
+    options = [
+      "rw"
+      "vers=4"
+    ];
+  };
+
   networking.hostName = "swrm3";
 }
