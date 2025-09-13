2ff623a7fa134a74a48a0e2bc5911bc845c05898
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-13 13:39:07 +0200

    chore: disable firewall

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -44,6 +44,7 @@
 
   # networking.firewall.enable = false;
   networking.firewall.trustedInterfaces = [ "docker0" ];
+  networking.firewall.enable = false;
   networking.firewall.allowedTCPPorts = [
     22
   ];
