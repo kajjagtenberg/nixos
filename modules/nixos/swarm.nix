9b7e2f68d5b5ffe4e36c02cc3886085b31b1f396
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-13 20:02:39 +0200

    chore: add more allowed ports for swarm

--- modules/nixos/swarm.nix
+++ modules/nixos/swarm.nix
@@ -6,9 +6,16 @@
     ./docker.nix
   ];
 
-  networking.firewall.allowedTCPPorts = [
-    2377
-    9000
-    9443
-  ];
+  networking.firewall = {
+    allowedTCPPorts = [
+      2377
+      9000
+      9443
+      7946
+    ];
+    allowedUDPPorts = [
+      4789
+      7946
+    ];
+  };
 }
