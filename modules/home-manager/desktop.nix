4ed7bc26739ff1d628c4e382b9ac623384fc1b3c
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-13 14:52:56 +0200

    chore: update alacritty look and feel

--- modules/home-manager/desktop.nix
+++ modules/home-manager/desktop.nix
@@ -1,5 +1,6 @@
 {
   pkgs,
+  lib,
   ...
 }:
 let
@@ -17,7 +18,6 @@ in
   programs.neovim.enable = true;
 
   home.packages = with pkgs; [
-    alacritty
     protonvpn-cli
   ];
 
@@ -99,6 +99,84 @@ in
           "browser.newtabpage.activity-stream.system.showSponsored" = lock-false;
           "browser.newtabpage.activity-stream.showSponsoredTopSites" = lock-false;
         };
+      };
+    };
+  };
+
+  programs.alacritty = {
+    enable = true;
+    settings = {
+      colors = {
+        bright = {
+          black = "0x737475";
+          blue = "0x959697";
+          cyan = "0xb15928";
+          green = "0x2e2f30";
+          magenta = "0xdadbdc";
+          red = "0xe6550d";
+          white = "0xfcfdfe";
+          yellow = "0x515253";
+        };
+        cursor = {
+          cursor = "0xb7b8b9";
+          text = "0x0c0d0e";
+        };
+        normal = {
+          black = "0x0c0d0e";
+          blue = "0x3182bd";
+          cyan = "0x80b1d3";
+          green = "0x31a354";
+          magenta = "0x756bb1";
+          red = "0xe31a1c";
+          white = "0xb7b8b9";
+          yellow = "0xdca060";
+        };
+        primary = {
+          # background = "0x0c0d0e";
+          background = "#0a0920";
+          foreground = "#b7b8b9";
+        };
+      };
+
+      cursor = {
+        unfocused_hollow = true;
+        style.blinking = "On";
+      };
+
+      window = {
+        dimensions = {
+          lines = 30;
+          columns = 150;
+        };
+        decorations = lib.mkMerge [
+          "Full"
+        ];
+        dynamic_padding = true;
+        padding = {
+          x = 4;
+          y = 4;
+        };
+        opacity = 0.65;
+      };
+
+      font = {
+        size = lib.mkMerge [
+          14
+        ];
+        normal = {
+          family = "JetBrainsMono Nerd Font";
+          style = "Regular";
+        };
+      };
+
+      keyboard = {
+        bindings = [
+          {
+            key = "Return";
+            mods = "Shift";
+            chars = "\n";
+          }
+        ];
       };
     };
   };
