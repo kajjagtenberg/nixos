9eb946059133198127599233876520825d6a096e
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-15 14:03:12 +0200

    chore: add media share to swarm servers

--- modules/nixos/nfs/docker.nix
+++ modules/nixos/nfs/docker.nix
@@ -4,4 +4,10 @@
     fsType = "nfs";
     options = [ "rw" "vers=4" ];
   };
+
+  fileSystems."/mnt/disposable/media" = {
+    device = "10.0.0.94:/mnt/disposable/media";
+    fsType = "nfs";
+    options = [ "rw" "vers=4" ];
+  };
 }
