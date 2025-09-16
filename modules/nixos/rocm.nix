841ab0babd975ea3d973738b437ba39912cb00d1
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-16 23:04:31 +0200

    chore: formatting

--- modules/nixos/rocm.nix
+++ modules/nixos/rocm.nix
@@ -2,9 +2,6 @@
   hardware.amdgpu.opencl.enable = true;
   hardware.opengl = {
     enable = true;
-    extraPackages = with pkgs; [
-      rocmPackages.rocm-smi
-      rocmPackages.rocminfo
-    ];
+    extraPackages = with pkgs; [ rocmPackages.rocm-smi rocmPackages.rocminfo ];
   };
 }
