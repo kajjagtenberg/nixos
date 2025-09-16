5d9ef72d64c3411fd862e7cbdd8397aa064dfed8
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-16 16:57:28 +0200

    chore: add rocm packages

--- modules/nixos/rocm.nix
+++ modules/nixos/rocm.nix
@@ -1,1 +1,15 @@
-{ ... }: { hardware.amdgpu.opencl.enable = true; }
+{ ... }: {
+  hardware.amdgpu.opencl.enable = true;
+  hardware.opengl = {
+    enable = true;
+    driSupport = true;
+    extraPackages = with pkgs; [
+      rocm-opencl-icd
+      rocm-opencl-runtime
+      rocm-smi
+      rocminfo
+    ];
+  };
+
+  services.rocm.enable = true;
+}
