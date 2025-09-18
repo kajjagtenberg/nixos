8815a216598e9d21ce19b7e87ead9e39104c052b
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-18 22:47:11 +0200

    chore: fix terminal tools setup

--- modules/home-manager/terminal/default.nix
+++ modules/home-manager/terminal/default.nix
@@ -9,11 +9,13 @@
 
   terminal.tools.enable = lib.mkDefault true;
 
-  programs = lib.mkIf config.terminal.tools.enable {
-    htop.enable = true;
-    vim.enable = true;
-    fzf.enable = true;
-    fd.enable = true;
-    starship.enable = true;
+  config = lib.mkIf config.terminal.tools.enable {
+    programs = {
+      htop.enable = true;
+      vim.enable = true;
+      fzf.enable = true;
+      fd.enable = true;
+      starship.enable = true;
+    };
   };
 }
