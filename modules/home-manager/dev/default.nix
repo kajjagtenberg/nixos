20fdb98e460545d9e7fa366e1254c8f9f224b125
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-18 22:29:51 +0200

    chore: move tmux to module

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -7,8 +7,10 @@
   imports = [
     ./neovim.nix
     ./git.nix
+    ./tmux.nix
   ];
 
   dev.neovim.enable = lib.mkDefault true;
   dev.git.enable = lib.mkDefault true;
+  dev.tmux.enable = lib.mkDefault true;
 }
