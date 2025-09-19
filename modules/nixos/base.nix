5bc842d9dd729dfa45c82adfb3d250bd6a6f0a1c
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-19 09:38:20 +0200

    chore: move openssh out

--- modules/nixos/base.nix
+++ modules/nixos/base.nix
@@ -8,8 +8,5 @@
 }:
 {
 
-  # Enable the OpenSSH daemon.
-  services.openssh.enable = true;
-
   system.stateVersion = "25.05";
 }
--- modules/nixos/system/default.nix
+++ modules/nixos/system/default.nix
@@ -12,5 +12,6 @@
     ./nix.nix
     ./packages.nix
     ./firewall.nix
+    ./openssh.nix
   ];
 }
