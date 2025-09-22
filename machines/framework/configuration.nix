2ba2a95073657ed7bc623594fa2f3440b850a00f
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-22 23:38:10 +0200

    refactor: remove redundant system-level SOPS config, handle via home-manager

--- machines/framework/configuration.nix
+++ machines/framework/configuration.nix
@@ -59,41 +59,7 @@
     };
   };
 
-  #######
 
-  # sops.defaultSopsFile = builtins.toString ./secrets/ssh.yaml;
-  # sops.defaultSopsFormat = "yaml";
-  #
-  # sops.age.keyFile = null;
-  #
-  # sops.secrets = {
-  #   "ssh_keys/framework" = {
-  #     path = "/home/${vars.username}/.ssh/id_ecdsa";
-  #   };
-  # };
-  sops = {
-    age.keyFile = "/home/kaj/.config/sops/age/keys.txt";
-
-    # defaultSopsFile = "${secretsFile}";
-    defaultSopsFile = ../../secrets/ssh.yaml;
-    # defaultSopsFormat = "yaml";
-    # validateSopsFiles = false;
-
-    # secrets = {
-    #   "ssh_keys/framework" = {
-    #     path = "/home/kaj/.ssh/id_ecdsa";
-    #   };
-    # };
-    #
-    secrets."ssh_keys/framework" = {
-      path = "/home/${vars.username}/.ssh/id_ecdsa";
-      owner = vars.username;
-    };
-    secrets.example = {
-      neededForUsers = true;
-      owner = "kaj";
-    };
-  };
 
   #######
 
