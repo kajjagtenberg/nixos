eeca2dca3e9d3f9f05674669ed025e6dd4e09835
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-22 22:39:06 +0200

    chore: update tmux config

--- modules/home-manager/dev/tmux.nix
+++ modules/home-manager/dev/tmux.nix
@@ -8,18 +8,55 @@
   options.dev.tmux.enable = lib.mkEnableOption "Enable tmux";
 
   config = lib.mkIf config.dev.tmux.enable {
-    home.file.".tmux.conf".source =
-      pkgs.fetchFromGitHub {
-        owner = "kajjagtenberg";
-        repo = "tmux";
-        rev = "d13ecfe";
-        sha256 = "sha256-O3yHOWlXKvJL+wToO53kSZK09BNGRRx7dk3blMpETXs=";
-      }
-      + "/tmux.conf";
-
     programs.tmux = {
       enable = true;
       baseIndex = 1;
+      mouse = true;
+      prefix = "C-Space";
+      keyMode = "vi";
+      extraConfig = ''
+        # Terminal overrides for true color support
+        set-option -sa terminal-overrides ",xterm*:Tc"
+
+        # Start panes at 1
+        set -g pane-base-index 1
+        set-window-option -g pane-base-index 1
+
+        # Renumber windows automatically
+        set-option -g renumber-windows on
+
+        # Vim style pane selection
+        bind h select-pane -L
+        bind j select-pane -D
+        bind k select-pane -U
+        bind l select-pane -R
+
+        # Use Alt-arrow keys without prefix key to switch panes
+        bind -n M-Left select-pane -L
+        bind -n M-Right select-pane -R
+        bind -n M-Up select-pane -U
+        bind -n M-Down select-pane -D
+
+        # Shift arrow to switch windows
+        bind -n S-Left  previous-window
+        bind -n S-Right next-window
+
+        # Shift Alt vim keys to switch windows
+        bind -n M-H previous-window
+        bind -n M-L next-window
+
+        # Vi mode keybindings for copy mode
+        bind-key -T copy-mode-vi v send-keys -X begin-selection
+        bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+        bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+        # Split windows in current directory
+        bind '"' split-window -v -c "#{pane_current_path}"
+        bind % split-window -h -c "#{pane_current_path}"
+
+        # Ensure CTRL+L works for clearing screen
+        bind -n C-l send-keys C-l
+      '';
       plugins = with pkgs.tmuxPlugins; [
         sensible
         yank
