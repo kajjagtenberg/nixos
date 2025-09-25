3889e7879f0597d80e67ccd3cca37cdc1c1f45b4
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-09-25 18:04:08 +0200

    chore: add lmstudio

---
+++ modules/home-manager/apps/lmstudio.nix
@@ -0,0 +1,15 @@
+{
+  lib,
+  pkgs,
+  config,
+  ...
+}:
+{
+  options.apps.lmstudio.enable = lib.mkEnableOption "Enable LM Studio";
+
+  config = lib.mkIf config.apps.lmstudio.enable {
+    home.packages = [
+      pkgs.lmstudio
+    ];
+  };
+}
--- profiles/home/desktop.nix
+++ profiles/home/desktop.nix
@@ -10,5 +10,6 @@
 
   browser.brave.enable = true;
   apps.spotify.enable = true;
+  apps.lmstudio.enable = true;
   terminal.alacritty.enable = true;
 }
