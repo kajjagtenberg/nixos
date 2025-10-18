fb84bb9f40a60fcc47eb64daa56318e87b38a0b3
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-10-18 11:47:02 +0200

    chore: add opencode back

--- modules/home-manager/dev/default.nix
+++ modules/home-manager/dev/default.nix
@@ -8,9 +8,11 @@
     ./neovim.nix
     ./git.nix
     ./tmux.nix
+    ./opencode.nix
   ];
 
   dev.neovim.enable = lib.mkDefault true;
   dev.git.enable = lib.mkDefault true;
   dev.tmux.enable = lib.mkDefault true;
+  dev.opencode.enable = lib.mkDefault true;
 }
