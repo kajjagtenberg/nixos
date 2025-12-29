35b2c00e1734162c1ee360ef6376e66352c69ebd
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-12-29 23:39:24 +0100

    chore: changed to stable channel

--- flake.nix
+++ flake.nix
@@ -2,10 +2,11 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-25.11";
+    nixpkgs-unstable.url = "github:nixos/nixpkgs/nixpkgs-unstable";
 
     home-manager = {
-      url = "github:nix-community/home-manager/master";
+      url = "github:nix-community/home-manager/release-25.11";
       inputs.nixpkgs.follows = "nixpkgs";
     };
 
@@ -15,40 +16,45 @@
     };
   };
 
-  outputs = {
-    self,
-    nixpkgs,
-    ...
-  } @ inputs: let
-    inherit (self) outputs;
-    vars = import ./vars.nix;
-    system = "x86_64-linux";
+  outputs =
+    {
+      self,
+      nixpkgs,
+      nixpkgs-unstable,
+      ...
+    }@inputs:
+    let
+      inherit (self) outputs;
+      vars = import ./vars.nix;
+      system = "x86_64-linux";
 
-    mkNixOSConfig = path:
-      nixpkgs.lib.nixosSystem {
-        specialArgs = {inherit inputs outputs vars;};
-        modules = [path];
+      mkNixOSConfig =
+        path:
+        nixpkgs.lib.nixosSystem {
+          specialArgs = { inherit inputs outputs vars; };
+          modules = [ path ];
+        };
+
+      mkHomeConfig = inputs.home-manager.lib.homeManagerConfiguration {
+        pkgs = nixpkgs.legacyPackages.${system};
+        extraSpecialArgs = { inherit inputs outputs vars; };
+        modules = [
+          ./modules/home-manager/base.nix
+          ./profiles/home/dev.nix
+          ./profiles/home/desktop.nix
+        ];
+      };
+    in
+    {
+      nixosConfigurations = {
+        framework = mkNixOSConfig ./machines/framework/configuration.nix;
+        swrm1 = mkNixOSConfig ./machines/swrm1/configuration.nix;
+        swrm2 = mkNixOSConfig ./machines/swrm2/configuration.nix;
+        swrm3 = mkNixOSConfig ./machines/swrm3/configuration.nix;
       };
 
-    mkHomeConfig = inputs.home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.${system};
-      extraSpecialArgs = {inherit inputs outputs vars;};
-      modules = [
-        ./modules/home-manager/base.nix
-        ./profiles/home/dev.nix
-        ./profiles/home/desktop.nix
-      ];
+      homeConfigurations = {
+        ${vars.username} = mkHomeConfig;
+      };
     };
-  in {
-    nixosConfigurations = {
-      framework = mkNixOSConfig ./machines/framework/configuration.nix;
-      swrm1 = mkNixOSConfig ./machines/swrm1/configuration.nix;
-      swrm2 = mkNixOSConfig ./machines/swrm2/configuration.nix;
-      swrm3 = mkNixOSConfig ./machines/swrm3/configuration.nix;
-    };
-
-    homeConfigurations = {
-      ${vars.username} = mkHomeConfig;
-    };
-  };
 }
