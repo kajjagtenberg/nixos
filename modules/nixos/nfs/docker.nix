827ab55dd8653f0462adedecfbda57d80d3ea2fc
Kaj Jagtenberg <info@kajjagtenberg.nl>
2025-12-29 23:44:35 +0100

    chore: disabled nfs mounts

--- modules/nixos/nfs/docker.nix
+++ modules/nixos/nfs/docker.nix
@@ -1,13 +1,14 @@
-{...}: {
-  fileSystems."/mnt/fast" = {
-    device = "10.0.0.94:/mnt/fast";
-    fsType = "nfs";
-    options = ["rw" "vers=4"];
-  };
-
-  fileSystems."/mnt/disposable/media" = {
-    device = "10.0.0.94:/mnt/disposable/media";
-    fsType = "nfs";
-    options = ["rw" "vers=4"];
-  };
+{ ... }:
+{
+  # fileSystems."/mnt/fast" = {
+  #   device = "10.0.0.94:/mnt/fast";
+  #   fsType = "nfs";
+  #   options = ["rw" "vers=4"];
+  # };
+  #
+  # fileSystems."/mnt/disposable/media" = {
+  #   device = "10.0.0.94:/mnt/disposable/media";
+  #   fsType = "nfs";
+  #   options = ["rw" "vers=4"];
+  # };
 }
